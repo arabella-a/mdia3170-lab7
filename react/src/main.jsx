59cdb4c31ea53af693c0f77c57e44c86fde2f2f9
Ara Bella <arabella@Aras-MacBook-Pro.local>
2024-11-24 16:20:23 -0800

    add basename to browserrouter

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -8,7 +8,7 @@ import Details from "./components/Details.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="mdia3170-lab7">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/countries" element={<Countries />}>
